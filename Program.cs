a74460d09e07bcf6cb5432200c4baec86393b9ad
Melek Karataş <melek022233@gmail.com>
2025-07-02 16:40:13 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// Ekrana yıldızlardan baklava dilimi yazdıran program
+
+Console.WriteLine("Kaç satır baklava girmek istiyorsunuz.");
+int satir = int.Parse(Console.ReadLine()), bosluk = satir / 2, yildiz = 1;
+satir = satir % 2 == 0 ? ++satir : satir;
+
+for (int i = 0; i < satir; i++)
+{
+    for (int j = 0; j < bosluk; j++)
+    {
+        Console.Write(" ");
+    }
+    for (int z = 0; z < yildiz; z++)
+    {
+        Console.Write("*");
+    }
+
+    if (i < satir / 2)
+    {
+        bosluk--;
+        yildiz += 2;
+    }
+    else
+    {
+        bosluk++;
+        yildiz -= 2;
+    }
+    Console.WriteLine();
+
+
+}
+
